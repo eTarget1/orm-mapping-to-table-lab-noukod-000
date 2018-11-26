99c0f0a2a0a81551ae618192b6c014a0e7d6ef53
clifford <clifford@noukod.com>
2018-11-26 08:32:52 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,6 @@
 class Student
-attr_accessor :name, :grade
-attr_reader id
+  attr_accessor :name, :grade
+  attr_reader :id
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
@@ -8,7 +8,19 @@ attr_reader id
     @name = name
     @grade = grade
     @id = id
-end
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
   def self.drop_table
     sql = <<-SQL
       DROP TABLE students
@@ -16,18 +28,7 @@ end
     DB[:conn].execute(sql)
   end
 
-  def self.create_table
-    sql = <<-SQL
-        CREATE TABLE IF NOT EXISTS students (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        grade TEXT
-        )
-      SQL
-      DB[:conn].execute(sql)
-    end
-
-    def save
+  def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
       VALUES (?, ?)
@@ -42,4 +43,5 @@ end
     student.save
     student
   end
+
 end
