21f1ca5a4f7ddd8bc38a3789f584e89a5105cb8e
clifford <clifford@noukod.com>
2018-11-26 08:31:52 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,7 +15,7 @@ end
     SQL
     DB[:conn].execute(sql)
   end
-  
+
   def self.create_table
     sql = <<-SQL
         CREATE TABLE IF NOT EXISTS students (
@@ -26,4 +26,20 @@ end
       SQL
       DB[:conn].execute(sql)
     end
+
+    def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+
+  def self.create(name:, grade:)
+    student = self.new(name, grade)
+    student.save
+    student
+  end
 end
