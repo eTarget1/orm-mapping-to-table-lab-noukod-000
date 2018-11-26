afd8a00f10735fd86696fb54c5041cbe0b7b023f
clifford <clifford@noukod.com>
2018-11-26 08:29:46 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,14 +10,14 @@ attr_reader id
     @id = id
 end
 
-def self.create_table
-  sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS students (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      grade TEXT
-      )
-    SQL
-    DB[:conn].execute(sql)
-  end
+  def self.create_table
+    sql = <<-SQL
+        CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+        )
+      SQL
+      DB[:conn].execute(sql)
+    end
 end
